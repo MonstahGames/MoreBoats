132d1f37a7a7f538f452dd3987cb5a1b9b3a326e
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-08 17:51:24 +0100

    Renamed to MoreBoats

---
+++ src/main/java/com/sigmastudios/mboats/MoreBoats.java
@@ -0,0 +1,19 @@
+package com.sigmastudios.mboats;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MoreBoats.MODID, version = MoreBoats.VERSION, name = MoreBoats.NAME)
+public class MoreBoats
+{
+    public static final String MODID = "mboats";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "More Boats";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
