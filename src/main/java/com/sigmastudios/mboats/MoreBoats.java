ea7be5738ea4af1dc17376ae2fa088fde0c82611
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-08 19:33:20 +0100

    Logger

--- src/main/java/com/sigmastudios/mboats/MoreBoats.java
+++ src/main/java/com/sigmastudios/mboats/MoreBoats.java
@@ -17,7 +17,9 @@ public class MoreBoats {
     public static final String VERSION = "1.0";
     public static final String NAME = "More Boats";
 
-    
+    public static Logger LOGGER = LogManager.getLogger(MODID.toUpperCase());
+
+    public static boolean DEBUG = true;
 
     public static final String CLProxy = "com.sigmastudios.mboats.proxy.ClientProxy";
     public static final String CProxy = "com.sigmastudios.mboats.proxy.CommonProxy";
@@ -29,12 +31,17 @@ public class MoreBoats {
     public static MoreBoats instance;
 
     public void preInit (FMLPreInitializationEvent event) {
+        LOGGER = event.getModLog();
+        if (DEBUG) {
+            LOGGER.info("Loading More Boats...");
+        }
         proxy.preInit(event);
     }
     public void init (FMLInitializationEvent event) {
         proxy.init(event);
     }
     public void postInit (FMLPostInitializationEvent event) {
+        LOGGER.info("More Boats Loaded!");
         proxy.postInit(event);
     }
 
