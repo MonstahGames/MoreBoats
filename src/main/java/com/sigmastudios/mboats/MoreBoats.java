5d18a46facd030e329373462cbedc2e536d9e3d7
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-08 19:31:59 +0100

    Proxy

--- src/main/java/com/sigmastudios/mboats/MoreBoats.java
+++ src/main/java/com/sigmastudios/mboats/MoreBoats.java
@@ -1,19 +1,41 @@
 package com.sigmastudios.mboats;
 
+import com.sigmastudios.mboats.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = MoreBoats.MODID, version = MoreBoats.VERSION, name = MoreBoats.NAME)
-public class MoreBoats
-{
+public class MoreBoats {
+
     public static final String MODID = "mboats";
     public static final String VERSION = "1.0";
     public static final String NAME = "More Boats";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
 
+    
+
+    public static final String CLProxy = "com.sigmastudios.mboats.proxy.ClientProxy";
+    public static final String CProxy = "com.sigmastudios.mboats.proxy.CommonProxy";
+
+    @SidedProxy(clientSide = CLProxy, serverSide = CProxy)
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static MoreBoats instance;
+
+    public void preInit (FMLPreInitializationEvent event) {
+        proxy.preInit(event);
     }
+    public void init (FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+    public void postInit (FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
 }
