c6285bcf79645ff5e520ee888da90b960cc849e0
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-08 20:37:32 +0100

    ModItems.java

---
+++ src/main/java/com/sigmastudios/mboats/item/ModItems.java
@@ -0,0 +1,16 @@
+package com.sigmastudios.mboats.item;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class ModItems {
+
+    public static void register (IForgeRegistry<Item> registry) {
+        //registry.register(items);
+    }
+    public static void registerModels () {
+
+    }
+
+
+}
